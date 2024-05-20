33f2058cf2347c07abb6e3d548dcb7d76196df12
Etienne Estrangin <etienne.estrangin@gmail.com>
2024-05-20 16:24:14 +0200

    3.14: auth microservice addition

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -1,10 +1,9 @@
 import { DatabaseModule, LoggerModule } from '@app/common';
-import { Module, ValidationPipe } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { ReservationDocument, ReservationSchema } from './models/reservation.schema';
 import { ReservationsController } from './reservations.controller';
 import { ReservationsRepository } from './reservations.repository';
 import { ReservationsService } from './reservations.service';
-import { APP_PIPE } from '@nestjs/core';
 
 @Module({
   imports: [DatabaseModule,
