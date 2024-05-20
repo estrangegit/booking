130812821aa7e27bb4a03637c66da898d5702aa3
Etienne Estrangin <etienne.estrangin@gmail.com>
2024-05-20 22:21:47 +0200

    3.15: authentication - passport library integration

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -1,14 +1,24 @@
 import { DatabaseModule, LoggerModule } from '@app/common';
+import * as Joi from 'joi';
 import { Module } from '@nestjs/common';
 import { ReservationDocument, ReservationSchema } from './models/reservation.schema';
 import { ReservationsController } from './reservations.controller';
 import { ReservationsRepository } from './reservations.repository';
 import { ReservationsService } from './reservations.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [DatabaseModule,
     DatabaseModule.forFeature([{ name: ReservationDocument.name, schema: ReservationSchema }]),
-    LoggerModule],
+    LoggerModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+        PORT: Joi.number().required()
+      }),
+    }),
+  ],
   controllers: [ReservationsController],
   providers: [ReservationsService, ReservationsRepository],
 })
